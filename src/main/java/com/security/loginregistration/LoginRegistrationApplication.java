02de4488853a02d58825cb5ba1a4dcf058e7eacc
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-06-24 22:12:59 +0600

    initial project configuration

---
+++ src/main/java/com/security/loginregistration/LoginRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.security.loginregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/loginregistration/LoginRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.loginregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
